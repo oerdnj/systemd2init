14046e3ca5f59680d36e70bd2e22968472ca5acd
Ondřej Surý <ondrej@sury.org>
2014-03-10 13:40:32 +0100

    Add GPL-3 license to the main file and add full license (COPYING)

--- systemd2init.sh
+++ systemd2init.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 # vim:noet:sw=4:ts=4
-# Author: Ondřej Surý <ondrej@sury.org>
+#    systemd2init - convert a systemd service file into init shell script
+#    Copyright (C) 2014 Ondřej Surý <ondrej@sury.org>
+#                       Lukáš Zapletal <lzap+git@redhat.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 lsconf() {
     RET=
